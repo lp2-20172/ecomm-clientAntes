e28a7ccb10e8a410d75e4c33857439fe78b22172
Angel Sullon <asullom@gmail.com>
2017-08-30 16:09:11 -0500

    depurando

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Footer from './Footer';
-import Header from './Header';
+import Footer from './home/Footer';
+import Header from './home/Header';
 
 class Home extends Component {
     constructor(props) {
